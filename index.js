149ccc82b7dc4be16c4d845ebcd1dc47ba9c3da0
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-01-17 17:49:14 +0600

    mailer added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { query } = require('express');
+
+
 require('dotenv').config()
+
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
@@ -19,6 +24,60 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2psefg9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function sendBookingEmail(booking) {
+    const { email, treatment, appointmentDate, slot } = booking;
+
+    const auth = {
+        auth: {
+          api_key: process.env.EMAIL_SEND_KEY,
+          domain: process.env.EMAIL_SEND_DOMAIN
+        }
+      }
+      
+      const transporter = nodemailer.createTransport(mg(auth));
+
+    
+    // let transporter = nodemailer.createTransport({
+    //     host: 'smtp.sendgrid.net',
+    //     port: 587,
+    //     auth: {
+    //         user: "apikey",
+    //         pass: process.env.SENDGRID_API_KEY
+    //     }
+    // });
+
+
+
+      console.log('sending email', email)
+
+
+      
+    transporter.sendMail({
+        from: "jhankar.mahbub2@gmail.com", // verified sender email
+        to: email || 'jhankar.mahbub2@gmail.com', // recipient email
+        subject: `Your appointment for ${treatment} is confirmed`, // Subject line
+        text: "Hello world!", // plain text body
+        html: `
+        <h3>Your appointment is confirmed</h3>
+        <div>
+            <p>Your appointment for treatment: ${treatment}</p>
+            <p>Please visit us on ${appointmentDate} at ${slot}</p>
+            <p>Thanks from Doctors Portal.</p>
+        </div>
+        
+        `, // html body
+    }, function (error, info) {
+        if (error) {
+            console.log('Email send error', error);
+        } else {
+            console.log('Email sent: ' + info);
+        }
+    });
+}
+
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
